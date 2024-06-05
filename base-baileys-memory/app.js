635c7a2190b33f0490afa370e2d5347b68b15bd6
Sanchez <SSanchez@emergencias.com.ar>
2024-06-05 13:44:03 -0700

    Agregar respuestas y mejoras en menu

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -8,7 +8,9 @@ const path = require('path');
 const fs = require('fs');
 const chat = require('./chatGPT');
 
-
+//######################################################################
+//################ SECCION PARA DECLARAR PATHS #########################
+//######################################################################
 
 const menuPath = path.join(__dirname, 'mensajes', 'menu.txt');
 const menu = fs.readFileSync(menuPath, 'utf-8');
@@ -16,22 +18,108 @@ const menu = fs.readFileSync(menuPath, 'utf-8');
 const problemasFrecuentesPath = path.join(__dirname, 'mensajes','problemasFrecuentes.txt')
 problemasFrecuentes = fs.readFileSync(problemasFrecuentesPath, 'utf-8');
 
-const serverAdminPath = path.join(__dirname, 'mensajes','serverAdmin.txt')
+const serverAdminPath = path.join(__dirname, 'mensajes','serverAdmin','serverAdmin.txt')
 serverAdmin = fs.readFileSync(serverAdminPath, 'utf-8');
 
-const networkingPath = path.join(__dirname, 'mensajes','networking.txt')
+const networkingPath = path.join(__dirname, 'mensajes', 'networking','networking.txt')
 networking = fs.readFileSync(networkingPath, 'utf-8');
 
-const gptPath = path.join(__dirname, 'mensajes','promptgpt.txt')
+const gptPath = path.join(__dirname, 'mensajes','GPT','promptgpt.txt')
 promptConsultas = fs.readFileSync(gptPath, 'utf-8');
 
+const serviceDeskPath = path.join(__dirname, 'mensajes','serviceDesk','service.txt')
+const service =  fs.readFileSync(serviceDeskPath, 'utf-8');
 
-const flowPrincipal = addKeyword (['Hola','hola'])
-    .addAnswer('Hola! Soy Sophie y estoy para ayudarte. Para ver las opciones escribi *Menu*')
+const rtaFortiPath = path.join(__dirname, 'mensajes', 'networking', 'rtaForti.txt')
+const rtaForti =  fs.readFileSync(rtaFortiPath, 'utf-8');
 
-// Definir el submenú para la opción 1
-const flowSubMenu1 = addKeyword(['1', '2', '3'])
-    .addAnswer('Has seleccionado una opción del submenú.');
+//######################################################################
+//################ SECCION PARA DECLARAR FLOWS #########################
+//######################################################################
+
+// **************** BIENVENIDA ***************************
+
+const flowPrincipal = addKeyword (['Hola','hola','Buenas','Holi','Holis'])
+    .addAnswer('🙋‍♀️Hola! Soy Sophie y estoy encantada que estes acá. ¿En que puedo ayudarte?\nPara emepzar escribi *Menu*')
+
+const flowWelcome = addKeyword(EVENTS.WELCOME)
+.addAnswer('🙋‍♀️Hola! Soy Sophie y estoy encantada que estes acá. ¿En que puedo ayudarte?\nPara emepzar escribi *Menu*')/*, {
+    delay: 100 
+}, async (ctx, ctxFn) => {
+    if (ctx.body.includes('casas')) {
+        await ctxFn.flowDynamic('Escribiste casas');
+    } else {
+        await ctxFn.flowDynamic('Escribiste otra cosa');
+    }
+};*/
+
+//************************NETWORKING ************************
+
+
+const flowRtaForti = addKeyword(EVENTS.ACTION)
+    .addAnswer(rtaForti)
+
+
+    const flowTicket = addKeyword(EVENTS.ACTION)
+    .addAnswer('Para hacer un ticker por favor ingresar a https://emergencias.sd.cloud.invgate.net/')
+    .addAnswer('h', {
+        media: 'https://emergencias.invgateusercontent.net/emergencias/uploads/attached_files/knowledge/4702/3f6cc75e84afb1416d4934b00269d4c3/Troubleshooting%20forti.pdf'
+    })
+    .addAnswer('Para volver a ver las opciones escribi *Menu*')
+
+const flowDesbloqueo = addKeyword(EVENTS.ACTION)
+    .addAnswer('Para desbloquear el usuario debes hacerlo desde AdSerfService: https://desbloqueo.emergencias.com.ar/authorization.do\nAca te paso el link con el instructivo: https://emergencias.sd.cloud.invgate.net/knowledgebase_articles/show/index/article_id/30')
+    
+    .addAnswer('Para volver a ver las opciones escribi *Menu*')
+
+const flowServerAdmin = addKeyword(EVENTS.ACTION)
+.addAnswer(
+    serverAdmin, {capture: true} , async (ctx, { gotoFlow, fallBack }) => {
+        if (ctx.body === '0') {
+            console.log(ctx.name)
+
+            return gotoFlow(flowProblemasFrecuentes);
+        }
+        // Aquí puedes agregar más lógica si necesitas manejar otras opciones dentro de flowNetworking
+        return fallBack('Respuesta no válida, por favor selecciona una opción válida o escribe 0 para volver al menú anterior.');
+    });
+
+
+
+const flowNetworking = addKeyword(EVENTS.ACTION)
+    .addAnswer(networking, { capture: true }, async (ctx, { gotoFlow, fallBack }) => {
+        switch (ctx.body) {
+            case '1':
+                return gotoFlow(flowRtaForti)
+  
+            case '0':
+                return gotoFlow(flowProblemasFrecuentes);         
+        }
+        
+
+    });
+
+const flowTelefonia = addKeyword(EVENTS.ACTION)
+.addAnswer('Te la debo');
+
+
+const flowCore = addKeyword(EVENTS.ACTION)
+.addAnswer('Te la debo');
+
+const flowServiceDesk2 = addKeyword(EVENTS.ACTION)
+.addAnswer('Primero debe probar con esa misma vincha en otra computadora o probar otra vincha en esa misma computadora. Tambien se puede probar la configuracion de volumen en el avaya y en la computadora haciendo click derecho en el icono del volvumen e ir a sonidos.')
+
+const flowConsultas = addKeyword(EVENTS.ACTION)
+    .addAnswer('Haz una consulta', { capture: true }, async (ctx, ctxFn) => {
+        const prompt = promptConsultas;
+        const consulta = ctx.body;
+        const answer = await chat(prompt, consulta);
+        await ctxFn.flowDynamic(answer.content);
+    });
+
+//######################################################################
+//################ SECCION PARA DECLARAR MENUS #########################
+//######################################################################
 
 // Modificar el flujo de teléfonos para incluir un submenú
 const flowProblemasFrecuentes = addKeyword(EVENTS.ACTION)
@@ -56,57 +144,28 @@ const flowProblemasFrecuentes = addKeyword(EVENTS.ACTION)
         })
     
 
-const flowTicket = addKeyword(EVENTS.ACTION)
-    .addAnswer('Para hacer un ticker por favor ingresar a https://emergencias.sd.cloud.invgate.net/')
-    .addAnswer('h', {
-        media: 'https://emergencias.invgateusercontent.net/emergencias/uploads/attached_files/knowledge/4702/3f6cc75e84afb1416d4934b00269d4c3/Troubleshooting%20forti.pdf'
-    })
-    .addAnswer('Para volver a ver las opciones escribi *Menu*')
-
-const flowDesbloqueo = addKeyword(EVENTS.ACTION)
-    .addAnswer('Para desbloquear el usuario debes hacerlo desde AdSerfService: https://desbloqueo.emergencias.com.ar/authorization.do\nAca te paso el link con el instructivo: https://emergencias.sd.cloud.invgate.net/knowledgebase_articles/show/index/article_id/30')
-    
-    .addAnswer('Para volver a ver las opciones escribi *Menu*')
-
-const flowServerAdmin = addKeyword(EVENTS.ACTION)
-.addAnswer(
-    serverAdmin, {capture: true} , async (ctx, { gotoFlow, fallBack }) => {
-        if (ctx.body === '0') {
-            return gotoFlow(flowProblemasFrecuentes);
-        }
-        // Aquí puedes agregar más lógica si necesitas manejar otras opciones dentro de flowNetworking
-        return fallBack('Respuesta no válida, por favor selecciona una opción válida o escribe 0 para volver al menú anterior.');
-    });
-
-
-
-const flowNetworking = addKeyword(EVENTS.ACTION)
-    .addAnswer(networking, { capture: true }, async (ctx, { gotoFlow, fallBack }) => {
-        if (ctx.body === '0') {
-            return gotoFlow(flowProblemasFrecuentes);
-        }
-        // Aquí puedes agregar más lógica si necesitas manejar otras opciones dentro de flowNetworking
-        return fallBack('Respuesta no válida, por favor selecciona una opción válida o escribe 0 para volver al menú anterior.');
-    });
-
-const flowTelefonia = addKeyword(EVENTS.ACTION)
-.addAnswer('Te la debo');
-
-
-const flowCore = addKeyword(EVENTS.ACTION)
-.addAnswer('Te la debo');
 
 const flowService = addKeyword(EVENTS.ACTION)
-.addAnswer('Te la debo');
+.addAnswer(service,
+    { capture: true },
+       async (ctx, { gotoFlow, fallBack }) => {
+           switch (ctx.body) {
+               case '1':
+                   return gotoFlow(flowServiceDesk2)
+               case '2':
+                   return gotoFlow(flowNetworking)
+               case '3':
+                   return gotoFlow(flowTelefonia)
+               case '4':
+                   return gotoFlow(flowCore)
+               case '5':
+                   return gotoFlow(flowService)
 
-const flowConsultas = addKeyword(EVENTS.ACTION)
-    .addAnswer('Esta es la seccion de consultas.')
-    .addAnswer('Haz una consulta', { capture: true }, async (ctx, ctxFn) => {
-        const prompt = promptConsultas;
-        const consulta = ctx.body;
-        const answer = await chat(prompt, consulta);
-        await ctxFn.flowDynamic(answer.content);
-    });
+               default:
+                   return fallBack('Respuesta no válida, por favor selecciona una opción del submenú.');
+           }
+       })
+
 
 const menuFlow = addKeyword(['Menu','menu','Menú','menú']).addAnswer(
     menu,
@@ -130,20 +189,11 @@ const menuFlow = addKeyword(['Menu','menu','Menú','menú']).addAnswer(
     }
 );
 
-const flowWelcome = addKeyword(EVENTS.WELCOME)
-    .addAnswer('Esta no es una palabra valida')/*, {
-        delay: 100
-    }, async (ctx, ctxFn) => {
-        if (ctx.body.includes('casas')) {
-            await ctxFn.flowDynamic('Escribiste casas');
-        } else {
-            await ctxFn.flowDynamic('Escribiste otra cosa');
-        }
-    };*/
+
 
 const main = async () => {
     const adapterDB = new MockAdapter();
-    const adapterFlow = createFlow([flowPrincipal,flowWelcome, menuFlow, flowProblemasFrecuentes, flowTicket, flowDesbloqueo, flowConsultas, flowSubMenu1, flowServerAdmin, flowNetworking, flowTelefonia, flowCore, flowService]);
+    const adapterFlow = createFlow([flowPrincipal,flowWelcome, menuFlow, flowProblemasFrecuentes, flowTicket, flowDesbloqueo, flowConsultas, flowServerAdmin, flowNetworking, flowTelefonia, flowCore, flowService, flowServiceDesk2,flowRtaForti]);
     const adapterProvider = createProvider(BaileysProvider);
 
     createBot({
