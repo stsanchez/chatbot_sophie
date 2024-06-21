6a4d423fca954ccede1a655f736a1aeba8c3d9d5
Sanchez <SSanchez@emergencias.com.ar>
2024-06-21 11:48:14 -0700

    cambios de prueba git

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -8,7 +8,7 @@ const path = require('path');
 const fs = require('fs');
 const chat = require('./chatGPT');
 
-//PROBANDO PULL GIT
+//PROBANDO PULL GITT
 //######################################################################
 //################ SECCION PARA DECLARAR PATHS #########################
 //######################################################################
